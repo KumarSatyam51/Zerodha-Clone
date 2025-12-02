69ec68608ae49a2e8a97bf986d0a329cb38badd0
kumar satyam <kumarsatayam51@gmail.com>
2025-12-02 15:43:14 +0530

    origin added :

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,8 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    // origin: 'http://localhost:5173',
+    origin:"http://zerodha-clone-osc9.vercel.app",
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
