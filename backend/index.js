749d63de80c43a307d5bcc4b817c51580c4d36dc
kumar satyam <kumarsatayam51@gmail.com>
2025-12-02 23:04:35 +0530

    fix cors error

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 
 app.use(cors({
     // origin: 'http://localhost:5173',
-    origin:"http://zerodha-clone-osc9.vercel.app",
+    origin:"https://zerodha-clone-osc9.vercel.app",
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
